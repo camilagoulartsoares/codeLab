e59f3efe2dfbc23eeb8f9f0d32403f3dc9cfbee3
Camila Soares <camila.soares19@hotmail.com>
2025-07-22 10:18:25 -0300

    Estilização post

--- codelab/src/components/PostCard.js
+++ codelab/src/components/PostCard.js
@@ -6,7 +6,7 @@ export default function PostCard({ post, isFirst }) {
   const [liked, setLiked] = useState(false);
 
   return (
-    <div className={`post-card ${isFirst ? 'active' : ''}`} data-aos="fade-up">
+<div className="post-card" data-aos="fade-up">
       <div className="post-like" onClick={() => setLiked(!liked)}>
         {liked ? <AiFillHeart className="icon liked" /> : <AiOutlineHeart className="icon" />}
         <span>{liked ? post.likes + 1 : post.likes}</span>
